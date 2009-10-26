dbfa50659c5232cc60b4cf157678b81f7a4e384c
Aleksandar Bradic <alek.br@gmail.com>
2009-10-26 03:12:50 +0100

    change conf

--- src/test/java/com/voidsearch/test/client/SimpleFeedqLoaderTest.java
+++ src/test/java/com/voidsearch/test/client/SimpleFeedqLoaderTest.java
@@ -30,7 +30,7 @@ public class SimpleFeedqLoaderTest {
     @Test
     public void nullTest() {
 
-        QueueTreeClient client = new QueueTreeClient("www.voidsearch.com:8080");
+        QueueTreeClient client = new QueueTreeClient("localhost:8080");
 
         try {
 
