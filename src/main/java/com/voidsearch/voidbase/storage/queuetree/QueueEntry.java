fdd086d95d120c94179ad2582195b54d82d40b17
alek_br <alek_br@alek-bradics-macbook-pro.local>
2009-10-30 23:46:12 +0100

    add timestamp to queue entry

---
+++ src/main/java/com/voidsearch/voidbase/storage/queuetree/QueueEntry.java
@@ -0,0 +1,30 @@
+package com.voidsearch.voidbase.storage.queuetree;
+
+public class QueueEntry {
+
+    private long timestamp;
+    private Object value;
+
+    public QueueEntry(Object value) {
+        timestamp = System.currentTimeMillis();
+        this.value = value;
+    }
+
+    // allow for overriding of getKey method
+    public String getKey()  {
+        return (String)value;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public String toString() {
+        return (String)value;
+    }
+
+}
