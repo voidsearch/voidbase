8c63204354c7186f846f0d9ea748a7f789120e57
kodi <dragan.bajcic@gmail.com>
2009-11-14 00:42:33 +0100

    added javascript cache interface

---
+++ src/webapp/web/js/Cache.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2009 VoidSearch.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+VOIDSEARCH.VoidBase.Cache = function() {
+    //private properties and methods
+
+
+    //public properties and methods
+    return{
+
+        get:function(key){
+
+
+        },
+
+        post:function(key, value){
+
+        },
+
+
+        readMetadata:function(key){
+
+
+        }
+
+
+    };
+}();
