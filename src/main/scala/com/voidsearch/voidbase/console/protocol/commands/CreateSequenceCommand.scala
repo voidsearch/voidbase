d5feca47aa3084dd632ca3cf63ef00bf6b8c5196
Aleksandar Bradic <alek.br@gmail.com>
2009-12-14 03:25:40 +0100

    add more commands for console

---
+++ src/main/scala/com/voidsearch/voidbase/console/protocol/commands/CreateSequenceCommand.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2009 VoidSearch.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.voidsearch.voidbase.console.protocol.commands
+
+import session.VoidBaseConsoleSession
+
+case class CreateSequenceCommand(session: VoidBaseConsoleSession, variableName: String, sequenceClass : String) extends VoidBaseConsoleCommand {
+
+  val SEQUENCE_GENERATOR_PACKAGE = "com.voidsearch.voidbase.quant.timeseries"
+  
+  def exec() = {
+
+    var className = sequenceClass
+    if (className.indexOf("(") > -1) {
+      className = className.substring(0,(className.indexOf("(")))
+    }
+
+    var canonicalPath = SEQUENCE_GENERATOR_PACKAGE + "." + className 
+
+    try {
+      val obj = Class.forName(canonicalPath);
+    }  catch {
+        case e => println("error creating sequence: " + className)
+    }
+
+    println("creating sequence: " + canonicalPath)
+
+
+  }
+
+}
---
+++ src/main/scala/com/voidsearch/voidbase/console/protocol/commands/ListCommand.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2009 VoidSearch.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.voidsearch.voidbase.console.protocol.commands
+
+
+import apps.queuetree.client.QueueTreeClient
+import session.VoidBaseConsoleSession
+import util.VoidBaseConsoleUtil
+
+case class ListCommand(session: VoidBaseConsoleSession) extends VoidBaseConsoleCommand {
+
+  def exec() = {
+
+    session.domain match {
+      case "queue" =>  listQTree()
+      case "qtree" =>  listQTree()
+      case "queuetree" => listQTree()
+      case _ => println("Invalid Domain: " + session.domain)
+    }
+
+  }
+
+  def listQTree() = {
+
+    var client = new QueueTreeClient(session.hostname)
+    println()
+    println("List of active qtree queues:")
+    println()
+
+    var res = client.list()
+    processList(VoidBaseConsoleUtil.getXML(res))
+
+  }
+
+  def processList(node: scala.xml.Node) = {
+    println(node.getClass)
+  }
+
+}
