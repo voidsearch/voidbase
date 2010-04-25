4f17ada7a9f10902790001422bcc4dd9e645df17
alek_br <alek_br@dynamic-oit-pu-dormnet-bb-a-132.princeton.edu>
2010-04-25 17:47:10 -0400

    Simple Facebook Graph API client

--- src/test/java/com/voidsearch/data/test/SimpleGraphAPIClientTest.java
+++ src/test/java/com/voidsearch/data/test/SimpleGraphAPIClientTest.java
@@ -26,10 +26,10 @@ import java.util.LinkedList;
 
 public class SimpleGraphAPIClientTest {
 
-    // sample token - expires every hour, obtained from links at :
+    // sample token - expires every hour, obtained from liks at :
     // http://developers.facebook.com/docs/api
     //
-    // (for testing only - you should obtain a proper access token)
+    // for testing only - you should obtain a proper access token
     //
 
     public String fbToken = "2227470867|2.7lsiCQ8_bWloy2L94B8vnA__.3600.1272232800-736212683|-bThDZvFpVP2SefvOibwP3hQ2es.";
